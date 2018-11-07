fcd87996acb6ad88d197dec1b2071ea835324e6d
Unknown <akbar.exp57@gmail.com>
2018-11-07 23:44:49 +0700

    Moving Composite

---
+++ Composite/Component.h
@@ -0,0 +1,13 @@
+#pragma once
+class Component
+{
+public:
+	void AddComponent();
+	void GetComponent();
+	void RemoveComponent();
+
+private:
+	component * Component;
+	string fungsi;
+};
+
---
+++ Composite/Graphic.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "Graphic.h"
+
+
+void Graphic::AddComponent()
+{
+}
+
+
+void Graphic::GetComponent()
+{
+}
+
+void Graphic::RemoveComponent()
+{
+}
+
